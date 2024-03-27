6ea3b25eaf9d9c73b1e96c05fb79b63f0def1928
SampleTester42 <165092455+SampleTester42@users.noreply.github.com>
2024-03-27 12:18:17 +0100

    Update Book.java

--- src/model/Book.java
+++ src/model/Book.java
@@ -2,15 +2,7 @@ package model;
 
 /**
  * This class represents a single book which is also a single entry in our database. It has a title and an author.
- * It inherits from class Medium so it must implement the method mediumToString()
+ * It should inherit from class Medium so it must implement the method mediumToString()
  */
-public class Book extends Medium {
-
-    // Create constructor
-    // Create getters and setters
-
-    @Override
-    public String mediumToString() {
-        //to implement
-    }
+public class Book {
 }
